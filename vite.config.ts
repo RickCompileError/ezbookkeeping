28d9812f5e30bee3f6ebe10bd33fd18c3b248b00
RickCompileError <gngn15401@gmail.com>
2026-06-30 22:35:48 +0800

    Fix: render doesn't include .git issue

--- vite.config.ts
+++ vite.config.ts
@@ -75,7 +75,7 @@ export default defineConfig(() => {
             __EZBOOKKEEPING_IS_PRODUCTION__: process.env['NODE_ENV'] === 'production',
             __EZBOOKKEEPING_VERSION__: JSON.stringify(packageFile.version),
             __EZBOOKKEEPING_BUILD_UNIX_TIME__: JSON.stringify(buildUnixTime),
-            __EZBOOKKEEPING_BUILD_COMMIT_HASH__: JSON.stringify(git.short()),
+            __EZBOOKKEEPING_BUILD_COMMIT_HASH__: (() => { try { return JSON.stringify(git.short()); } catch { return JSON.stringify(''); } })(),
             __EZBOOKKEEPING_CONTRIBUTORS__: JSON.stringify(contributorsFile),
             __EZBOOKKEEPING_LICENSE__: JSON.stringify(licenseContent),
             __EZBOOKKEEPING_THIRD_PARTY_LICENSES__: JSON.stringify(thirdPartyLicenseFile)
